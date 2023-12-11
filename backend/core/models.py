40ab75d21fa9f9fcb9bdcfc095ecc0774c529dee
Saging Con Yelo <96810918+BohemianRasberry@users.noreply.github.com>
2023-12-11 17:56:02 +0800

    updating models

--- backend/core/models.py
+++ backend/core/models.py
@@ -7,7 +7,7 @@ class Clinic(models.Model):
     
 class Patient(models.Model):
     SEX_CHOICES = (('F', 'Female'), ('M', 'Male'))
-    patient_id = models.AutoField(primary_key=True)
+    #patient_id = models.AutoField(primary_key=True)
     patient_firstname = models.CharField(max_length=50, unique=True)
     patient_familyname = models.CharField(max_length=50, unique=True)
     patient_age = models.IntegerField()
@@ -16,26 +16,29 @@ class Patient(models.Model):
     patient_AMD = models.CharField(max_length=50)
     
 class Pathologist(models.Model):
-    pathologist_id = models.AutoField(primary_key=True)
+    #pathologist_id = models.AutoField(primary_key=True)
     pathologist_name = models.CharField(max_length=50, unique=True)
     pathologist_prc_id = models.CharField(max_length=10, unique=True)
     
+    default_ptahologist_id = 1  # You can set any default value here
+    
+
 class Medtech(models.Model):
-    medtech_id = models.AutoField(primary_key=True)
+    #medtech_id = models.AutoField(primary_key=True)
     medtech_name = models.CharField(max_length=50, unique=True)
     medtech_prc_id = models.CharField(max_length=10, unique=True)
-    
+
 class Specimen(models.Model):
-    specimen_id = models.AutoField(primary_key=True)
-    medtech_id = models.ForeignKey(Medtech, on_delete=models.CASCADE)
-    patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE) 
+    #specimen_id = models.AutoField(primary_key=True)
+    medtech_id = models.ForeignKey(Medtech, on_delete=models.SET_NULL, null=True)
+    patient_id = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True) 
     
 class Transaction(models.Model):
-    transaction_id = models.AutoField(primary_key=True)
-    test_code = models.IntegerField()
-    patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    pathologist_id = models.ForeignKey(Pathologist, on_delete=models.CASCADE)
-    specimen_id = models.ForeignKey(Specimen, on_delete=models.CASCADE)
+    #transaction_id = models.AutoField(primary_key=True)
+    test_code = models.IntegerField(null=True)
+    patient_id = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
+    pathologist_id = models.ForeignKey(Pathologist, on_delete=models.SET_NULL, null=True)
+    specimen_id = models.ForeignKey(Specimen, on_delete=models.SET_NULL, null=True)
     datetime = models.DateTimeField()
     
 class Complete_Blood_Count(models.Model):
@@ -46,6 +49,10 @@ class Complete_Blood_Count(models.Model):
     hemaglobin = models.FloatField()
     mcv = models.FloatField()
     mch = models.FloatField()
+    mchc = models.FloatField()
+    platelet_count = models.FloatField()
+    
+    
 
 
     
