d1d36d2998ba36a88a1afd25fb8a22b7fdfc8f85
MD-4Good0 <agudo.mdn@gmail.com>
2023-12-14 16:12:31 +0800

    askdakdjakdjasdkjas mhm mhm

--- backend/frontend/src/Components/HDNPatientInfo/HDNPI.jsx
+++ backend/frontend/src/Components/HDNPatientInfo/HDNPI.jsx
@@ -1,23 +1,38 @@
+import React from 'react';
 import logo_icon from '../Assets/Logo.png';
 import './HDNPI.css';
  
+const patientsData = [
+    {
+        familyName: 'Doe',
+        firstName: 'David',
+        dateOfBirth: '1990-01-01',
+        age: '32',
+        sex: 'M',
+        patientId: 'P123456',
+    },
+];
+
 const HDNPI = () => {
     return (
         <div className="hdnpi-container">
             <div className="hdnpi-title">
                 <img src={logo_icon} alt="Logo" />
-                <div className="hdnpi-title">Laboratory Test Portal</div>
+                <div>Laboratory Test Portal</div> {/* Fixed: Removed className to prevent conflict */}
             </div>
 
             <div className="hdnpi-info-container">
-                <div className="hdnpi-patient-info-header-row">
-                    <div className="hdnpi-p-i-h-t-indiv">Family Name: </div>
-                    <div className="hdnpi-p-i-h-t-indiv">First Name: </div>
-                    <div className="hdnpi-p-i-h-t-indiv">Date of Birth: </div>
-                    <div className="hdnpi-p-i-h-t-indiv">Age </div>
-                    <div className="hdnpi-p-i-h-t-indiv">Sex: </div>
-                    <div className="hdnpi-p-i-h-t-indiv">Patient ID: </div>
-                </div>
+                {/* Patient Data Rows */}
+                {patientsData.map((patient, index) => (
+                    <div key={index} className="hdnpi-patient-info-header-row">
+                        <div className="hdnpi-p-i-h-t-indiv">Family Name: {patient.familyName}</div>
+                        <div className="hdnpi-p-i-h-t-indiv">First Name: {patient.firstName}</div>
+                        <div className="hdnpi-p-i-h-t-indiv">Date of Birth: {patient.dateOfBirth}</div>
+                        <div className="hdnpi-p-i-h-t-indiv">Age: {patient.age}</div>
+                        <div className="hdnpi-p-i-h-t-indiv">Sex: {patient.sex}</div>
+                        <div className="hdnpi-p-i-h-t-indiv">Patient ID: {patient.patientId}</div>
+                    </div>
+                ))}
 
                 <div className="hdnpi-patient-detailed-info-header-row">
                     <div className="hdnpi-patients-header">
