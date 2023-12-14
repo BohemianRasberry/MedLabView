00c88b7b59873ed171e16cf337638eb74f0ec14b
Saging Con Yelo <96810918+BohemianRasberry@users.noreply.github.com>
2023-12-14 17:08:20 +0800

    working hdnpi

--- backend/frontend/src/Components/HDNPatientInfo/HDNPI.jsx
+++ backend/frontend/src/Components/HDNPatientInfo/HDNPI.jsx
@@ -3,7 +3,7 @@ import logo_icon from '../Assets/Logo.png';
 import './HDNPI.css';
 import { useParams } from 'react-router-dom';
 import { db } from '../../Firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, query, collection, where, getDocs } from 'firebase/firestore'; // Import the necessary functions
  
 const labTestData = [
     {
@@ -76,9 +76,9 @@ const labTestData = [
 
 
 const HDNPI = () => {
-
     const { patientId } = useParams();
     const [patientData, setPatientData] = useState(null);
+    const [transactions, setTransactions] = useState([]);
 
     useEffect(() => {
         const fetchPatientData = async () => {
@@ -95,11 +95,23 @@ const HDNPI = () => {
             }
         };
 
+        const fetchTransactions = async () => {
+            try {
+                const transactionsQuery = query(collection(db, 'transaction'), where('patientid', '==', patientId));
+                const transactionsSnapshot = await getDocs(transactionsQuery);
+                setTransactions(transactionsSnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            } catch (error) {
+                console.error('Error fetching transactions:', error.message);
+            }
+        };
+
         fetchPatientData();
+        fetchTransactions();
     }, [patientId]);
 
     return (
         <div className="hdnpi-container">
+            
             <div className="hdnpi-title">
                 <img src={logo_icon} alt="Logo" />
                 <div>Laboratory Test Portal</div> {/* Fixed: Removed className to prevent conflict */}
@@ -117,37 +129,37 @@ const HDNPI = () => {
                         <div className="hdnpi-p-i-h-t-indiv">Patient ID: {patientData.patientid}</div>
                     </div>
                 )}
+            <div className="hdnpi-patient-detailed-info-header-row">
+                <div className="hdnpi-patients-header">
+                    <div className="hdnpi-p-h">Date & Time Given</div>
+                    <div className="hdnpi-p-h-separator">|</div>
+                    <div className="hdnpi-p-h">Specimen Number</div>
+                    <div className="hdnpi-p-h-separator">|</div>
+                    <div className="hdnpi-p-h">Laboratory Test</div>
+                    <div className="hdnpi-p-h-separator">|</div>
+                    <div className="hdnpi-p-h">Laboratory Test ID</div>
+                </div>
 
-                <div className="hdnpi-patient-detailed-info-header-row">
-                    <div className="hdnpi-patients-header">
-                        <div className="hdnpi-p-h">Date & Time Given</div>
-                        <div className="hdnpi-p-h-separator">|</div>
-                        <div className="hdnpi-p-h">Specimen Number</div>
-                        <div className="hdnpi-p-h-separator">|</div>
-                        <div className="hdnpi-p-h">Laboratory Test</div>
-                        <div className="hdnpi-p-h-separator">|</div>
-                        <div className="hdnpi-p-h">Laboratory Test ID</div>
-                    </div>
-
-                    <div className="hdnpi-patients-table-container">
-                        <div className="hdnpi-p-t-c-table">
-                            {labTestData.map((test, index) => (
-                                <div key={index} className="hdnpi-patients-row">
-                                    <div className="hdnpi-p-h-cell">{test.dateTimeGiven}</div>
-                                    <div className="hdnpi-p-h-separator">|</div>
-                                    <div className="hdnpi-p-h-cell">{test.specimenNumber}</div>
-                                    <div className="hdnpi-p-h-separator">|</div>
-                                    <div className="hdnpi-p-h-cell">{test.labTest}</div>
-                                    <div className="hdnpi-p-h-separator">|</div>
-                                    <div className="hdnpi-p-h-cell">{test.labTestId}</div>
-                                </div>
-                            ))}
-                        </div>
+                <div className="hdnpi-patients-table-container">
+                    <div className="hdnpi-p-t-c-table">
+                        {/* Display transactions */}
+                        {transactions.map((transaction) => (
+                            <div key={transaction.id} className="hdnpi-patients-row">
+                                <div className="hdnpi-p-h-cell">{transaction.datetime}</div>
+                                <div className="hdnpi-p-h-separator">|</div>
+                                <div className="hdnpi-p-h-cell">{transaction.specimenid}</div>
+                                <div className="hdnpi-p-h-separator">|</div>
+                                <div className="hdnpi-p-h-cell">{transaction.testcode}</div>
+                                <div className="hdnpi-p-h-separator">|</div>
+                                <div className="hdnpi-p-h-cell">{transaction.transactionid}</div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
+        </div>
     );
-}
+};
 
 export default HDNPI;
